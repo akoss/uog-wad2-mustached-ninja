d4fb38eeee22eef21543f758c52efb8a84ed6511
Akos Szente <akos.szente@egroup.hu>
2015-03-24 04:42:14 +0100

    Ascending order of manufacturers

--- car_maniacs_project/carwebsite/templatetags/carwebsite_extras.py
+++ car_maniacs_project/carwebsite/templatetags/carwebsite_extras.py
@@ -5,4 +5,4 @@ register = template.Library()
 
 @register.inclusion_tag('carwebsite/mans.html')
 def get_manufacturer_list(man=None):
-    return {'mans': Manufacturer.objects.all(), 'act_man': man}
+    return {'mans': Manufacturer.objects.order_by('name'), 'act_man': man}
